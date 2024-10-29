74a92d6acff2b374b166f1e793b6d40263cc67f8
Péter Király <pkiraly@gwdg.de>
2024-10-29 13:24:44 +0100

    Add path to API response

--- retrieve-files-from-api.sh
+++ retrieve-files-from-api.sh
@@ -1,8 +1,8 @@
 source ./common.sh
 
-echo "id,filename,storageIdentifier" | sed -r 's/,/\t/g' > data.csv
+echo "id,filename,storageIdentifier,path" | sed -r 's/,/\t/g' > data.csv
 curl -s -H X-Dataverse-key:$API_KEY "$SERVER_URL/api/datasets/:persistentId/?persistentId=$PERSISTENT_IDENTIFIER" \
-  | jq '.data.latestVersion.files[] | .dataFile | .id, .filename, .storageIdentifier, "___"' \
+  | jq '.data.latestVersion.files[] | .dataFile.id, .dataFile.filename, .dataFile.storageIdentifier, .directoryLabel, "___"' \
   | paste -s -d "\t" \
   | sed -r 's/\t"___"\t?/\n/g' \
   >> data.csv
