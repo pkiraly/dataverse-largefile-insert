ed8cf8c1b3ab71522dc2b1be3aca3ca4d734868c
Peter Kiraly <pkiraly@gwdg.de>
2024-10-29 13:15:32 +0100

    Fix list-file-size

--- list-file-size.sh
+++ list-file-size.sh
@@ -12,7 +12,9 @@ echo "size,path" | sed -r 's/,/\t/g' > $OUTPUT
 # find $UPLOAD_DIR -type f -exec ls -la {} \; | awk '{print $5 "\t" $9}'  | sed  "s|$UPLOAD_DIR||g" >> $OUTPUT
 
 find $UPLOAD_DIR -type f -exec ls -la {} \; \
- | awk '{sub($1".*"$4, ""); sub($2".*"$4, "\t"); print}' \
+ | awk '{sub($1".*"$4, "");sub($2".*"$4, "\t"); print}' \
  | sed 's,^ ,,' \
+ | sed 's,\t ,\t,' \
+ | sed 's, \t,\t,' \
  | sed  "s|$UPLOAD_DIR/||g" \
  >> $OUTPUT
